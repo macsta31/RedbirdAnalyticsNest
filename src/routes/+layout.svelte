3b53dcf23628673df1d369b2f05db6d43d2d7e2c
macsta31 <mackstathis@gmail.com>
2023-08-15 14:19:05 -0400

    basic layout and styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    import "../styles.css"
+    export let data: LayoutData;
+</script>
+
+<main>
+    <header>
+        <h1>Redbirds Analytics Nest</h1>
+    </header>
+    <div>
+        <slot />
+    </div>
+
+    <footer>
+        Product of Apostolos Mandelos & Mackenze Stathis
+    </footer>
+</main>
+
+<style>
+    header{
+        background-color:var(--primary);
+        padding: 1rem;
+        display: grid;
+        place-content: center;
+    }
+
+    h1{
+        padding-top: 5px;
+    }
+
+    main{
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+    div{
+        flex: 1 auto;
+    }
+
+    footer{
+        display: grid;
+        place-content: center;
+        padding: 2rem;
+        font-size: 0.75rem;
+        border-top: 1px solid rgb(174, 174, 174);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,63 @@
-<h1>Redbird Analytics Next</h1>
-<h3>RAHHHHHHH🦅🇺🇸🦅🇺🇸🦅🇺🇸</h3>
+<script>
+    import '../styles.css'
+</script>
+
+<main>
+    <section class="upload">
+        <h2>Sheet Upload Form</h2>
+        <form action="">
+            <label for="inputSheet">
+                input here:
+                <input type="file">
+            </label>
+            <button type="submit">Submit Sheet</button>
+        </form>
+    </section>
+</main>
+
+<style>
+    main{
+        display: flex;
+        flex-direction: column;
+        flex: 1 auto;
+        width: 100%;
+    }
+
+    .upload{
+        width: 100%;
+        border: 1px solid black;
+        padding: 1rem;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+
+    form{
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+    }
+
+    label{
+        display: flex;
+        gap: 1rem;
+        font-size: 1.5rem;
+    }
+
+    button{
+        width: max-content;
+        background-color: var(--primary);
+        padding: 0.5rem 1rem;
+        border-radius: 1rem;
+        font-size: 1.5rem;
+    }
+
+    h2{
+        font-size: 2rem;
+    }
+
+    input{
+        font-size: 1.5rem;
+    }
+</style>
 
